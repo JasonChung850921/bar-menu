33b5e9fc87f23cedf1305dce1ad0d900f390119d
Kevin Kao <namedkk126@gmail.com>
2021-03-18 22:40:26 +0800

    working ui for reservations

--- src/App.js
+++ src/App.js
@@ -8,16 +8,35 @@ const panes = [
   {
     menuItem: "訂位",
     render: () => (
-      <Tab.Pane>
+      <Tab.Pane className="m-0" attached={false}>
         <Reservation />
       </Tab.Pane>
     ),
   },
-  { menuItem: "點餐", render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
-  { menuItem: "結帳", render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
+  {
+    menuItem: "點餐",
+    render: () => (
+      <Tab.Pane className="m-0" attached={false}>
+        Tab 2 Content
+      </Tab.Pane>
+    ),
+  },
+  {
+    menuItem: "結帳",
+    render: () => (
+      <Tab.Pane className="m-0" attached={false}>
+        Tab 3 Content
+      </Tab.Pane>
+    ),
+  },
 ];
 const App = () => {
-  return <Tab panes={panes} />;
+  return (
+    <Tab
+      menu={{ color: "teal", inverted: true, attached: true, tabular: false }}
+      panes={panes}
+    />
+  );
 };
 
 export default App;
--- src/components/Reservation.js
+++ src/components/Reservation.js
@@ -1,8 +1,146 @@
-import React from "react";
-import { Formik } from "formik";
+import React, { useState } from "react";
+import { Formik, Form as FormikForm, Field } from "formik";
+import { Grid, Segment, Button, Header, Form } from "semantic-ui-react";
+import * as Yup from "yup";
+import DateFnsUtils from "@date-io/date-fns"; // choose your lib
+import { DateTimePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import locale from "date-fns/locale/zh-TW";
 
 const Reservation = () => {
-  return <div>asdfasfd</div>;
+  const registrationSchema = Yup.object().shape({
+    username: Yup.string().required("Please provide your username."),
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Please provide an email."),
+    password: Yup.string()
+      .required("Please provide a password.")
+      .min(5, "Password is too short - should be 5 chars minimum."),
+  });
+  const [radio, setRadio] = useState("sm");
+  const [startDate, setStartDate] = useState(new Date());
+  const handleChangeRadio = (_, { value }) => setRadio(value);
+
+  return (
+    <Formik
+      initialValues={{
+        username: "",
+        email: "",
+        password: "",
+        confirmation: "",
+      }}
+      validationSchema={registrationSchema}
+      onSubmit={(values, { setSubmitting }) => {
+        setSubmitting(true);
+        console.log(values);
+      }}
+    >
+      {({
+        isSubmitting,
+        isValid,
+        dirty,
+        errors,
+        touched,
+        getFieldProps,
+        handleSubmit,
+      }) => {
+        return (
+          <Grid textAlign="center" verticalAlign="middle" className="app">
+            <Grid.Column style={{ maxWidth: 450 }}>
+              <Header as="h1" icon color="teal" textAlign="center">
+                訂位
+              </Header>
+              <Form as={FormikForm} onSubmit={handleSubmit} size="large">
+                <Segment>
+                  <Field
+                    {...getFieldProps("username")}
+                    error={
+                      errors.username &&
+                      touched.username && {
+                        content: errors.username,
+                        pointing: "above",
+                      }
+                    }
+                    as={Form.Input}
+                    fluid
+                    name="username"
+                    icon="user"
+                    iconPosition="left"
+                    placeholder="訂位姓名"
+                    type="text"
+                  />
+                  <Field
+                    {...getFieldProps("email")}
+                    error={
+                      errors.email &&
+                      touched.email && {
+                        content: errors.email,
+                        pointing: "above",
+                      }
+                    }
+                    as={Form.Input}
+                    fluid
+                    name="email"
+                    icon="users"
+                    iconPosition="left"
+                    placeholder="訂位人數"
+                    type="email"
+                  />
+                  <MuiPickersUtilsProvider utils={DateFnsUtils} locale={locale}>
+                    <Field
+                      style={{ width: "100%" }}
+                      {...getFieldProps("email")}
+                      error={
+                        errors.email &&
+                        touched.email && {
+                          content: errors.email,
+                          pointing: "above",
+                        }
+                      }
+                      label="訂位日期"
+                      as={DateTimePicker}
+                      name="date"
+                      value={startDate}
+                      onChange={setStartDate}
+                      disablePast
+                      minutesStep={5}
+                    />
+                  </MuiPickersUtilsProvider>
+                  <Form.Group
+                    className="mt-3"
+                    style={{ display: "flex", justifyContent: "space-evenly" }}
+                  >
+                    <label>桌型: </label>
+                    <Form.Radio
+                      label="大"
+                      value="lg"
+                      checked={radio === "lg"}
+                      onChange={handleChangeRadio}
+                    />
+                    <Form.Radio
+                      label="小"
+                      value="sm"
+                      checked={radio === "sm"}
+                      onChange={handleChangeRadio}
+                    />
+                  </Form.Group>
+                  <Button
+                    disabled={isSubmitting || !(isValid && dirty)}
+                    className={isSubmitting ? "loading mt-3" : "mt-3"}
+                    type="submit"
+                    color="teal"
+                    fluid
+                    size="large"
+                  >
+                    送出
+                  </Button>
+                </Segment>
+              </Form>
+            </Grid.Column>
+          </Grid>
+        );
+      }}
+    </Formik>
+  );
 };
 
 export default Reservation;
