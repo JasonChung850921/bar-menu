2ee0737f33cbc63c6f9a8b034379769d7434fe36
Kevin Kao <namedkk126@gmail.com>
2021-03-22 17:58:13 +0800

    update to prod

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,3 @@
-// const baseUrl = "https://warm-caverns-76487.herokuapp.com";
-const baseUrl = "http://localhost:1337";
+const baseUrl = "https://warm-caverns-76487.herokuapp.com";
+// const baseUrl = "http://localhost:1337";
 export default baseUrl;
