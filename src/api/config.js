5b8b3a9f1203437a128a7afd3a2bb945fe184f98
JasonChung850921 <80828348+JasonChung850921@users.noreply.github.com>
2021-03-25 00:48:42 +0800

    Update config.js
    update url for db/backend

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,3 @@
-const baseUrl = "https://warm-caverns-76487.herokuapp.com";
+const baseUrl = "https://bar-ordering-backend.herokuapp.com/";
 // const baseUrl = "http://localhost:1337";
 export default baseUrl;
