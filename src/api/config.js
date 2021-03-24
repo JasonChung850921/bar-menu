83b7e91fb5ff30d9b029e771344658d6dff15d69
JasonChung850921 <80828348+JasonChung850921@users.noreply.github.com>
2021-03-25 00:52:19 +0800

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,3 @@
-const baseUrl = "https://bar-ordering-backend.herokuapp.com/";
+const baseUrl = "https://bar-ordering-backend.herokuapp.com";
 // const baseUrl = "http://localhost:1337";
 export default baseUrl;
